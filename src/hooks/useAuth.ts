626a5b528f3e048bc0b9a21f969684de08032a1a
k-imoto <git@k-i-style.com>
2025-02-26 13:53:56 +0900

    lib,hooks作成

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { Session } from "@supabase/supabase-js";
+import supabase from "@/lib/supabase";
+
+const useAuth = () => {
+  const [session, setSession] = useState<Session | null>(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const { data: authData } = supabase.auth.onAuthStateChange((_, session) => {
+      setSession(session);
+    });
+
+    return () => authData.subscription.unsubscribe();
+  }, []);
+
+  const signInWithGithub = async () => {
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: "github",
+      });
+      if (error) {
+        setError(error.message);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else if (typeof error === "string") {
+        setError(error);
+      } else {
+        console.error("GitHubとの連携に失敗しました。");
+      }
+    }
+  };
+
+  const profileFromGithub: {
+    nickName: string;
+    avatarUrl: string;
+  } = {
+    nickName: session?.user?.user_metadata.user_name,
+    avatarUrl: session?.user?.user_metadata.avatar_url,
+  };
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return {
+    session,
+    error,
+    profileFromGithub,
+    signInWithGithub,
+    signOut,
+  };
+};
+
+export default useAuth;
